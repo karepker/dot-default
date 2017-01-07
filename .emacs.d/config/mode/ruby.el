a5d4fb8572128736655893725153b423b92e40c2
Kar Epker <karepker@gmail.com>
2017-01-07 02:33:54 -0800

    Changed ruby indent to two spaces.

--- .emacs.d/config/mode/ruby.el
+++ .emacs.d/config/mode/ruby.el
@@ -3,3 +3,4 @@
             '("\\.\\(?:cap\\|gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist
             '("\\(?:Brewfile\\|Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+(setq ruby-indent-level 2)
