c8921a33780be5621879947142dc81448e10a794
Kar Epker <karepker@gmail.com>
2017-05-08 22:55:12 -0700

    Move emacs markdown settings into hook so they actually work.

--- .emacs.d/config/mode/markdown.el
+++ .emacs.d/config/mode/markdown.el
@@ -6,13 +6,13 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 
 (setq-default markdown-list-indent-width 2)
-(setq evil-shift-width markdown-list-indent-width)
-(setq tab-width markdown-list-indent-width)
 
 ;; Turn on spell checking on the fly in markdown.
 (add-hook 'gfm-mode-hook
 		  (lambda ()
             (flyspell-mode 1)
+            (setq evil-shift-width markdown-list-indent-width)
+            (setq tab-width markdown-list-indent-width)
             (set (make-local-variable 'whitespace-style) nil)))
 
 
